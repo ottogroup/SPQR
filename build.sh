53a38fa2965091f99cea249d2f0317aa6e07bccd
mnxfst <mnxfst@gmail.com>
2015-04-07 23:13:49 +0200

    folders modified build script added

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+cd spqr-parent
+mvn clean; mvn install;
+
+cd ..
+cd spqr-build
+mvn clean; mvn install;
+cd ..
+
+mkdir -p /opt/transport/spqr/spqr-node/lib
+mkdir -p /opt/transport/spqr/spqr-node/bin
+mkdir -p /opt/transport/spqr/spqr-node/etc
+mkdir -p /opt/transport/spqr/spqr-node/repo
+mkdir -p /opt/transport/spqr/spqr-node/log
+mkdir -p /opt/transport/spqr/spqr-node/queues
+
+cd spqr-node/src/main/config
+cp * /opt/transport/spqr/spqr-node/etc/
+cd ../../..
+
+cd target/lib
+cp * /opt/transport/spqr/spqr-node/lib
+cd ../..
+
+cd src/main/scripts
+cp * /opt/transport/spqr/spqr-node/bin/
