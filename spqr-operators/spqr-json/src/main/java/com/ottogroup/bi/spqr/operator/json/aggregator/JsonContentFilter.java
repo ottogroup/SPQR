8c42b43383a1aad8c4999e1f8946e68fdd0c5bb4
Christian Kreutzfeldt <christian.kreutzfeldt@ottogroup.com>
2015-04-08 11:50:25 +0200

    json content filter added

---
+++ spqr-operators/spqr-json/src/main/java/com/ottogroup/bi/spqr/operator/json/aggregator/JsonContentFilter.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright 2015 Otto (GmbH & Co KG)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ottogroup.bi.spqr.operator.json.aggregator;
+
+import java.util.Properties;
+
+import com.ottogroup.bi.spqr.exception.ComponentInitializationFailedException;
+import com.ottogroup.bi.spqr.exception.RequiredInputMissingException;
+import com.ottogroup.bi.spqr.pipeline.component.MicroPipelineComponent;
+import com.ottogroup.bi.spqr.pipeline.component.MicroPipelineComponentType;
+import com.ottogroup.bi.spqr.pipeline.component.operator.DirectResponseOperator;
+import com.ottogroup.bi.spqr.pipeline.message.StreamingDataMessage;
+
+/**
+ * Filters the content of incoming {@link StreamingDataMessage} for specific content. All non-matching messages
+ * are removed from the pipeline, all matching messages get passed on to the next {@link MicroPipelineComponent} 
+ * @author mnxfst
+ * @since Apr 8, 2015
+ */
+public class JsonContentFilter implements DirectResponseOperator {
+
+	private String id = null;
+	private int totalNumOfMessages = 0;
+
+	/**
+	 * @see com.ottogroup.bi.spqr.pipeline.component.MicroPipelineComponent#initialize(java.util.Properties)
+	 */
+	public void initialize(Properties properties) throws RequiredInputMissingException,
+			ComponentInitializationFailedException {
+	}
+
+	/**
+	 * @see com.ottogroup.bi.spqr.pipeline.component.MicroPipelineComponent#shutdown()
+	 */
+	public boolean shutdown() {
+		return true;
+	}
+
+	/**
+	 * @see com.ottogroup.bi.spqr.pipeline.component.operator.DirectResponseOperator#onMessage(com.ottogroup.bi.spqr.pipeline.message.StreamingDataMessage)
+	 */
+	public StreamingDataMessage[] onMessage(StreamingDataMessage message) {
+		return null;
+	}
+
+	/**
+	 * @see com.ottogroup.bi.spqr.pipeline.component.MicroPipelineComponent#getType()
+	 */
+	public MicroPipelineComponentType getType() {
+		return MicroPipelineComponentType.DIRECT_RESPONSE_OPERATOR;
+	}
+
+	/**
+	 * @see com.ottogroup.bi.spqr.pipeline.component.operator.Operator#getTotalNumOfMessages()
+	 */
+	public long getTotalNumOfMessages() {
+		return this.totalNumOfMessages;
+	}
+
+	/**
+	 * @see com.ottogroup.bi.spqr.pipeline.component.MicroPipelineComponent#setId(java.lang.String)
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * @see com.ottogroup.bi.spqr.pipeline.component.MicroPipelineComponent#getId()
+	 */
+	public String getId() {
+		return this.id;
+	}
+
+}
