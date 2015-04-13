b10958d2aae4ba2fc1441fb6656e443b1b949949
mnxfst <mnxfst@gmail.com>
2015-04-13 23:24:06 +0200

    basic test cases for node client added - this is no rocket science neither is it complete eclipse m2e handling

--- spqr-resman/src/main/java/com/ottogroup/bi/spqr/resman/node/SPQRNodeClient.java
+++ spqr-resman/src/main/java/com/ottogroup/bi/spqr/resman/node/SPQRNodeClient.java
@@ -25,6 +25,8 @@ import org.apache.log4j.Logger;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ottogroup.bi.spqr.exception.RequiredInputMissingException;
 import com.ottogroup.bi.spqr.node.resource.pipeline.MicroPipelineInstantiationResponse;
+import com.ottogroup.bi.spqr.node.resource.pipeline.MicroPipelineShutdownResponse;
+import com.ottogroup.bi.spqr.node.resource.pipeline.MicroPipelineShutdownResponse.MicroPipelineShutdownState;
 import com.ottogroup.bi.spqr.pipeline.MicroPipelineConfiguration;
 import com.ottogroup.bi.spqr.pipeline.MicroPipelineValidationResult;
 import com.ottogroup.bi.spqr.pipeline.MicroPipelineValidator;
@@ -86,6 +88,38 @@ public class SPQRNodeClient {
 		if(logger.isDebugEnabled())
 			logger.debug("rest client[protocol="+protocol+", host="+remoteHost+", servicePort="+servicePort+", adminPort="+adminPort+"]");
 	}
+
+	/**
+	 * Initializes the node client using the provided input
+	 * @param protocol
+	 * @param remoteHost
+	 * @param servicePort
+	 * @param adminPort
+	 * @param client
+	 * @throws RequiredInputMissingException
+	 */
+	public SPQRNodeClient(final String protocol, final String remoteHost, final int servicePort, final int adminPort, final Client client) throws RequiredInputMissingException {
+		
+		///////////////////////////////////////////////////////////////
+		// validate input
+		if(StringUtils.isBlank(protocol))
+			throw new RequiredInputMissingException("Missing required protocol");
+		if(StringUtils.isBlank(remoteHost))
+			throw new RequiredInputMissingException("Missing required remote host");
+		if(servicePort < 1)
+			throw new RequiredInputMissingException("Missing required service port");
+		if(adminPort < 1)
+			throw new RequiredInputMissingException("Missing required admin port");
+		//
+		///////////////////////////////////////////////////////////////
+		
+		this.processingNodeServiceBaseUrl = new StringBuffer(protocol).append("://").append(remoteHost).append(":").append(servicePort).toString();
+		this.processingNodeAdminBaseUrl = new StringBuffer(protocol).append("://").append(remoteHost).append(":").append(adminPort).toString();
+		this.restClient = client;
+		
+		if(logger.isDebugEnabled())
+			logger.debug("rest client[protocol="+protocol+", host="+remoteHost+", servicePort="+servicePort+", adminPort="+adminPort+"]");
+	}
 	
 	/**
 	 * Instantiates the referenced pipeline on the remote processing node
@@ -97,9 +131,7 @@ public class SPQRNodeClient {
 	 */
 	public MicroPipelineInstantiationResponse instantiatePipeline(final MicroPipelineConfiguration pipelineConfiguration) 
 			throws RequiredInputMissingException, IOException, RemoteClientConnectionFailedException {
-		
-
-		
+				
 		///////////////////////////////////////////////////////
 		// validate input for not being null as the content
 		MicroPipelineValidationResult cfgValidationResult = this.pipelineConfigurationValidator.validate(pipelineConfiguration);
@@ -117,7 +149,7 @@ public class SPQRNodeClient {
 			logger.debug("Instantiating pipeline [id="+pipelineConfiguration.getId()+"] on processing node " + url.toString());
 		
 		try {
-			WebResource pipelineInstantiationResource = restClient.resource(url.toString());
+			WebResource pipelineInstantiationResource = this.restClient.resource(url.toString());
 			ClientResponse response = pipelineInstantiationResource.accept(MediaType.APPLICATION_JSON).type(MediaType.APPLICATION_JSON).post(ClientResponse.class, message);
 			String responseString = response.getEntity(String.class);
 			return this.jsonMapper.readValue(responseString, MicroPipelineInstantiationResponse.class);
@@ -135,33 +167,74 @@ public class SPQRNodeClient {
 	 * @throws IOException
 	 * @throws RemoteClientConnectionFailedException
 	 */
-//	public MicroPipelineInstantiationResponse updateOrInstantiatePipeline(final MicroPipelineConfiguration pipelineConfiguration) 
-//			throws RequiredInputMissingException, IOException, RemoteClientConnectionFailedException {
-//		
-//		///////////////////////////////////////////////////////
-//		// validate input
-//		if(pipelineConfiguration == null)
-//			throw new RequiredInputMissingException("Missing required configuration");
-//		if(StringUtils.isBlank(pipelineConfiguration.getId()))
-//			throw new RequiredInputMissingException("Missing required pipeline identifier");
-//		// TODO validate pipeline to avoid sending it to remote instances
-//		//
-//		///////////////////////////////////////////////////////
-//		
-//
-//		String message = jsonMapper.writeValueAsString(new MicroPipelineInstantiationRequest(pipelineConfiguration)); 
-//		StringBuffer url = new StringBuffer(this.processingNodeServiceBaseUrl).append("/pipelines/").append(pipelineConfiguration.getId());
-//		
-//		if(logger.isDebugEnabled()) 
-//			logger.debug("Updating or instantiating pipeline [id="+pipelineConfiguration.getId()+"] on processing node " + url.toString());
-//		
-//		try {
-//			WebResource pipelineInstantiationResource = client.resource(url.toString());
-//			ClientResponse response = pipelineInstantiationResource.accept(MediaType.APPLICATION_JSON).type(MediaType.APPLICATION_JSON).put(ClientResponse.class, message);
-//			String responseString = response.getEntity(String.class);
-//			return this.jsonMapper.readValue(responseString, MicroPipelineInstantiationResponse.class);
-//		} catch(Exception e) {
-//			throw new RemoteClientConnectionFailedException("Failed to establish a connection with the remote resource manager [url="+url.toString()+"]. Error: " + e.getMessage());
-//		}		
-//	}	
+	public MicroPipelineInstantiationResponse updateOrInstantiatePipeline(final MicroPipelineConfiguration pipelineConfiguration) 
+			throws RequiredInputMissingException, IOException, RemoteClientConnectionFailedException {
+		
+		///////////////////////////////////////////////////////
+		// validate input
+		MicroPipelineValidationResult cfgValidationResult = this.pipelineConfigurationValidator.validate(pipelineConfiguration);
+		if(cfgValidationResult != MicroPipelineValidationResult.OK)
+			return new MicroPipelineInstantiationResponse("", cfgValidationResult, "Failed to validate pipeline configuration");
+		if(StringUtils.isBlank(pipelineConfiguration.getId()))
+			return new MicroPipelineInstantiationResponse("", MicroPipelineValidationResult.MISSING_PIPELINE_ID, "Failed to generated unique pipeline identifier");
+		//
+		///////////////////////////////////////////////////////
+
+		String message = jsonMapper.writeValueAsString(pipelineConfiguration); 
+		StringBuffer url = new StringBuffer(this.processingNodeServiceBaseUrl).append("/pipelines/").append(pipelineConfiguration.getId());
+		
+		if(logger.isDebugEnabled()) 
+			logger.debug("Updating or instantiating pipeline [id="+pipelineConfiguration.getId()+"] on processing node " + url.toString());
+		
+		try {
+			WebResource pipelineInstantiationResource = this.restClient.resource(url.toString());
+			ClientResponse response = pipelineInstantiationResource.accept(MediaType.APPLICATION_JSON).type(MediaType.APPLICATION_JSON).put(ClientResponse.class, message);
+			String responseString = response.getEntity(String.class);
+			return this.jsonMapper.readValue(responseString, MicroPipelineInstantiationResponse.class);
+		} catch(Exception e) {
+			throw new RemoteClientConnectionFailedException("Failed to establish a connection with the remote resource manager [url="+url.toString()+"]. Error: " + e.getMessage());
+		}		
+	}
+	
+	/**
+	 * Shuts down the referenced pipeline on the remote processing node
+	 * @param pipelineId
+	 * @return
+	 * @throws RequiredInputMissingException
+	 * @throws RemoteClientConnectionFailedException
+	 */
+	public MicroPipelineShutdownResponse shutdown(final String pipelineId) throws RequiredInputMissingException, RemoteClientConnectionFailedException {
+		
+		///////////////////////////////////////////////////////
+		// validate input
+		if(StringUtils.isBlank(pipelineId))
+			return new MicroPipelineShutdownResponse(pipelineId, MicroPipelineShutdownState.PIPELINE_ID_MISSING, "Missing required pipeline identifier");
+		//
+		///////////////////////////////////////////////////////
+
+		StringBuffer url = new StringBuffer(this.processingNodeServiceBaseUrl).append("/pipelines/").append(pipelineId);
+
+		if(logger.isDebugEnabled()) 
+			logger.debug("Deleting pipeline [id="+pipelineId+"] on processing node " + url.toString());
+		
+		try {
+			WebResource pipelineShutdownResource = this.restClient.resource(url.toString());
+			ClientResponse response = pipelineShutdownResource.accept(MediaType.APPLICATION_JSON).type(MediaType.APPLICATION_JSON).delete(ClientResponse.class);
+			String responseString = response.getEntity(String.class);
+			return this.jsonMapper.readValue(responseString, MicroPipelineShutdownResponse.class);
+		} catch(Exception e) {
+			throw new RemoteClientConnectionFailedException("Failed to establish a connection with the remote resource manager [url="+url.toString()+"]. Error: " + e.getMessage());
+		}		
+
+	}
+
+	protected String getProcessingNodeServiceBaseUrl() {
+		return processingNodeServiceBaseUrl;
+	}
+
+	protected String getProcessingNodeAdminBaseUrl() {
+		return processingNodeAdminBaseUrl;
+	}
+	
+	
 }
---
+++ spqr-resman/src/test/java/com/ottogroup/bi/spqr/resman/node/SPQRNodeClientTest.java
@@ -0,0 +1,175 @@
+/**
+ * Copyright 2015 Otto (GmbH & Co KG)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ottogroup.bi.spqr.resman.node;
+
+
+
+import javax.ws.rs.core.MediaType;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ottogroup.bi.spqr.exception.RequiredInputMissingException;
+import com.ottogroup.bi.spqr.node.resource.pipeline.MicroPipelineInstantiationResponse;
+import com.ottogroup.bi.spqr.pipeline.MicroPipelineConfiguration;
+import com.ottogroup.bi.spqr.pipeline.MicroPipelineValidationResult;
+import com.ottogroup.bi.spqr.pipeline.component.MicroPipelineComponentConfiguration;
+import com.ottogroup.bi.spqr.pipeline.component.MicroPipelineComponentType;
+import com.ottogroup.bi.spqr.pipeline.queue.StreamingMessageQueueConfiguration;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.WebResource.Builder;
+
+/**
+ * Test case for {@link SPQRNodeClient}
+ * @author mnxfst
+ * @since Apr 13, 2015
+ *
+ */
+public class SPQRNodeClientTest {
+
+	/**
+	 * Test case for {@link SPQRNodeClient#SPQRNodeClient(String, String, int, int, com.sun.jersey.api.client.Client)} being provided
+	 * an empty protocol
+	 */
+	@Test
+	public void testConstructor_withEmptyProtocol() {
+		try {
+			new SPQRNodeClient("", "localhost", 8080, 9090);
+			Assert.fail("Missing required input");
+		} catch(RequiredInputMissingException e) {
+			// expected
+		}
+	}
+
+	/**
+	 * Test case for {@link SPQRNodeClient#SPQRNodeClient(String, String, int, int, com.sun.jersey.api.client.Client)} being provided
+	 * an empty remote host
+	 */
+	@Test
+	public void testConstructor_withEmptyRemoteHost() {
+		try {
+			new SPQRNodeClient("http", "", 8080, 9090);
+			Assert.fail("Missing required input");
+		} catch(RequiredInputMissingException e) {
+			// expected
+		}
+	}
+
+	/**
+	 * Test case for {@link SPQRNodeClient#SPQRNodeClient(String, String, int, int, com.sun.jersey.api.client.Client)} being provided
+	 * an invalid service port
+	 */
+	@Test
+	public void testConstructor_withInvalidServicePort() {
+		try {
+			new SPQRNodeClient("http", "", -1, 9090);
+			Assert.fail("Missing required input");
+		} catch(RequiredInputMissingException e) {
+			// expected
+		}
+	}
+
+	/**
+	 * Test case for {@link SPQRNodeClient#SPQRNodeClient(String, String, int, int, com.sun.jersey.api.client.Client)} being provided
+	 * an invalid admin port
+	 */
+	@Test
+	public void testConstructor_withInvalidAdminPort() {
+		try {
+			new SPQRNodeClient("http", "", 8080, -1);
+			Assert.fail("Missing required input");
+		} catch(RequiredInputMissingException e) {
+			// expected
+		}
+	}
+
+	/**
+	 * Test case for {@link SPQRNodeClient#SPQRNodeClient(String, String, int, int, com.sun.jersey.api.client.Client)} being provided
+	 * valid settings
+	 */
+	@Test
+	public void testConstructor_withValidInput() throws Exception {
+		SPQRNodeClient client = new SPQRNodeClient("http", "localhost", 8080, 8081);
+		Assert.assertEquals("Values must be equal", "http://localhost:8080", client.getProcessingNodeServiceBaseUrl());
+		Assert.assertEquals("Values must be equal", "http://localhost:8081", client.getProcessingNodeAdminBaseUrl());
+	}
+	
+	/**
+	 * Test case for {@link SPQRNodeClient#instantiatePipeline(com.ottogroup.bi.spqr.pipeline.MicroPipelineConfiguration)} being
+	 * provided null to test the validation of incoming configurations. As the validator is checked somewhere else it is not
+	 * necessary to do all in-depth thorough checking again as the constraints do not change
+	 */
+	@Test
+	public void testInstantiatePipeline_withEmptyConfiguration() throws Exception {
+		SPQRNodeClient client = new SPQRNodeClient("http", "localhost", 8080, 8081);
+		Assert.assertEquals("Values must be equal", MicroPipelineValidationResult.MISSING_CONFIGURATION, client.instantiatePipeline(null).getState());
+	}
+	
+	/**
+	 * Test case for {@link ProcessingNodeClient#instantiatePipeline(com.ottogroup.bi.asap.node.pipeline.MicroPipelineConfiguration)}
+	 * being provided valid input
+	 */
+	@Test
+	public void testInstantiatePipeline_withValidInput() throws Exception {
+
+		ObjectMapper jsonMapper = new ObjectMapper();
+		String processingNodeHost = "processing-node-localhost";
+		int processingNodeServicePort = 7070;
+		int processingNodeAdminPort = 7071;
+		MicroPipelineConfiguration sampleConfiguration = new MicroPipelineConfiguration();
+		sampleConfiguration.setId("Test");
+		
+		MicroPipelineComponentConfiguration componentCfg = new MicroPipelineComponentConfiguration();
+		componentCfg.setId("test-component");
+		componentCfg.setName("test");
+		componentCfg.setSettings(null);
+		componentCfg.setToQueue("test-destination-queue");
+		componentCfg.setType(MicroPipelineComponentType.SOURCE);
+		componentCfg.setVersion("test-version");
+		
+		sampleConfiguration.getComponents().add(componentCfg);
+		sampleConfiguration.getQueues().add(new StreamingMessageQueueConfiguration("test-destination-queue"));
+		
+		String pipelineInstantiationRequest = jsonMapper.writeValueAsString(sampleConfiguration);
+		String pipelineInstantiationResponse = jsonMapper.writeValueAsString(new MicroPipelineInstantiationResponse(sampleConfiguration.getId(), MicroPipelineValidationResult.OK, ""));
+		String url = new StringBuffer("http://").append(processingNodeHost).append(":").append(processingNodeServicePort).append("/pipelines/").append(sampleConfiguration.getId()).toString();
+		
+		//////////////////////////////////////////////////////////
+		// prepare mocked entities
+		Client clientMock = Mockito.mock(Client.class);		
+		WebResource webResourceMock = Mockito.mock(WebResource.class);
+		ClientResponse clientResponseMock = Mockito.mock(ClientResponse.class);
+		Builder b1Mock = Mockito.mock(Builder.class);
+		Builder b2Mock = Mockito.mock(Builder.class);		
+		
+		Mockito.when(clientMock.resource(url)).thenReturn(webResourceMock);
+		Mockito.when(webResourceMock.accept(MediaType.APPLICATION_JSON)).thenReturn(b1Mock);
+		Mockito.when(b1Mock.type(MediaType.APPLICATION_JSON)).thenReturn(b2Mock); // required to support chained methods for retrieving client response
+		Mockito.when(b2Mock.post(ClientResponse.class, pipelineInstantiationRequest)).thenReturn(clientResponseMock);
+		Mockito.when(clientResponseMock.getEntity(String.class)).thenReturn(pipelineInstantiationResponse);
+		//
+		//////////////////////////////////////////////////////////				
+		
+		SPQRNodeClient client = new SPQRNodeClient("http", processingNodeHost, processingNodeServicePort, processingNodeAdminPort, clientMock);
+		MicroPipelineInstantiationResponse response = client.instantiatePipeline(sampleConfiguration);
+		Assert.assertNotNull("Response must not be null", response);
+		Assert.assertEquals("State must be 'OK'", MicroPipelineValidationResult.OK, response.getState());
+	}
+}
